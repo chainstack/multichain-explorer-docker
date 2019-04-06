361f6c71dd1115c11f6b8739f01ac231717a5fbb
Alex Khaerov <hayorov@users.noreply.github.com>
2019-04-06 23:43:41 +0300

    Do not craft config for explorer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash -x
 
-echo "Setup explorer.conf"
-cat << EOF > /app/explorer.conf
-port 2750
-host 0.0.0.0
-datadir += [{
-        "dirname": "${DATADIR}",
-        "loader": "default",
-        "chain": "MultiChain ${CHAINNAME}",
-        "policy": "MultiChain"
-        }]
-dbtype = sqlite3
-connect-args = dockerchain.explorer.sqlite
-EOF
-
 echo "Run the explorer"
-python -m Mce.abe --config /app/explorer.conf --commit-bytes 100000 --no-serve && 
-python -m Mce.abe --config /app/explorer.conf
+python -m Mce.abe --config ${EXPLORER_CFG} --commit-bytes 100000 --no-serve && 
+python -m Mce.abe --config ${EXPLORER_CFG}
